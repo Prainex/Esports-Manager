1aa52b748071390b8a20a2d3338019d3f491d1aa
Arvind Majumder <majumderarvind@gmail.com>
2025-04-30 18:45:45 -0400

    Added Bug reports, slash commands feature, and assign tasks feature. Few bugs on all the commands added so far

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging
 import os
-
 import discord
 from discord import app_commands
 from discord.ext import commands
@@ -10,8 +9,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-OWNER_ID = int(os.getenv('ORG_OWNER'))
+OWNER = int(os.getenv('BUGS_ID'))
 TOKEN = os.getenv('DISCORD_TOKEN')
+TASK_CHANNEL= int(os.getenv('TASKS_ID'))
+BOT_LOG = int(os.getenv('LOG_CHANNEL'))
 
 handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
 
@@ -20,13 +21,28 @@ client = commands.Bot(command_prefix='!', intents=discord.Intents.all())
 @client.command()
 async def goodboy(ctx):
     await ctx.send("I'm a Goodboy, bark bark")
-    await
+
+@client.tree.command(name="assign_task", description="Assign a task to a user")
+async def create_task(interaction: discord.Interaction, task: str, deadline: str, assignee: str, description: str):
+    await interaction.response.send_message(f'Creating task: {task}')
+    await client.get_channel(BOT_LOG).send(f'Assigned {assignee} to complete task: {task}. They are expected to finish by {deadline}. Description: {description}')
+    await client.get_channel(TASK_CHANNEL).send(f'Assigned {assignee} to complete task: {task}. They are expected to finish by {deadline}. Description: {description}')
+    # await client.get_user(assignee).send(f'You have been assigned to complete task: {task}. You are expected to finish by {deadline}. Description: {description}')
 
 
+@client.tree.command(name="report_bug", description="report a bug")
+async def report(interaction: discord.Interaction, command: str, description: str):
+    await client.get_channel(OWNER).send(f'Bug reported: {command} \n Description: {description}')
+    await interaction.response.send_message("Your bug report has been submitted successfully!")
 
 @client.event
 async def on_ready():
     print(f'We have logged onto, {client.user}, and are ready to go!')
+    try:
+        synced = await client.tree.sync()
+        print(f"Synced {len(synced)} commands")
+    except Exception as e:
+        print(f"Error syncing commands: {e}")
 
 @client.event
 async def on_member_join(member):
